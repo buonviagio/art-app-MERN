dc24be3ec6be31cc729f0a29d5ef69c8f6aedc88
Shapovalov Dmytro <buonviagio@gmail.com>
2025-02-18 21:48:59 +0100

    adding skeleton to detailed page

--- client/src/pages/detailsPage/DetailsPage.tsx
+++ client/src/pages/detailsPage/DetailsPage.tsx
@@ -5,11 +5,16 @@ import { useEffect, useState } from "react";
 import CommentsSection from "../../components/commentsSection/CommentsSection";
 import "./DetailsPage.css";
 import { baseURL } from "../../utils/baseURL";
+import Placeholder from "react-bootstrap/esm/Placeholder";
+import { Card } from "react-bootstrap";
 
 export default function DetailsPage() {
   const { artditail } = useParams();
   const navigate = useNavigate();
   const [artDetails, setArtDetails] = useState<ArtsObjectResponce | null>(null);
+
+  // Track loading state
+  const [loading, setLoading] = useState(true);
 
   const handleBackClick = () => {
     navigate(-1);
@@ -32,6 +37,9 @@ export default function DetailsPage() {
       }
     } catch (error) {
       console.error("Error fetching art details:", error);
+    } finally {
+      setLoading(false);
+      console.log("LOADER FALSE:>> ");
     }
   };
   useEffect(() => {
@@ -56,30 +64,60 @@ export default function DetailsPage() {
       <div className="row">
         {/* Artwork Image */}
         <div className="col-md-6">
-          <img
-            src={artDetails?.picture.secure_url}
-            alt={artDetails?.nameOfThePainting}
-            className="img-fluid rounded"
-          />
+          {loading ? (
+            <div
+              style={{ height: "300px", width: "100%", background: "#959697" }}
+            ></div>
+          ) : (
+            <img
+              src={artDetails?.picture.secure_url}
+              alt={artDetails?.nameOfThePainting}
+              className="img-fluid rounded"
+            />
+          )}
         </div>
 
         {/* Artwork Details */}
-        <div className="col-md-6">
-          <h2 className="text-primary">{artDetails?.nameOfThePainting}</h2>
-          <h5 className="text-muted">By {artDetails?.nameOfTheAuthor}</h5>
-          <p>
-            <strong>Style:</strong> {artDetails?.style}
-          </p>
-          <p>
-            <strong>Year:</strong> {artDetails?.year}
-          </p>
-          <p>
-            <strong>Location:</strong> {artDetails?.location}
-          </p>
-          <p>
-            <strong>Description:</strong> {artDetails?.description}
-          </p>
-        </div>
+        {loading ? (
+          <div className="col-md-6">
+            <Placeholder xs={12} bg="secondary" />
+            <Placeholder xs={4} size="sm" />
+            <p>
+              <strong>Style:</strong> <Placeholder xs={4} animation="glow" />
+            </p>
+            <p>
+              <strong>Year:</strong> <Placeholder xs={2} animation="glow" />
+            </p>
+            <p>
+              <strong>Location:</strong> <Placeholder xs={4} animation="glow" />
+            </p>
+            <p>
+              <strong>Description:</strong>
+              <Placeholder xs={12} size="sm" animation="glow" />
+              <Placeholder xs={12} size="sm" animation="glow" />
+              <Placeholder xs={12} size="sm" animation="glow" />
+              <Placeholder xs={12} size="sm" animation="glow" />
+              <Placeholder xs={12} size="sm" animation="glow" />
+            </p>
+          </div>
+        ) : (
+          <div className="col-md-6">
+            <h2 className="text-primary">{artDetails?.nameOfThePainting}</h2>
+            <h5 className="text-muted">By {artDetails?.nameOfTheAuthor}</h5>
+            <p>
+              <strong>Style:</strong> {artDetails?.style}
+            </p>
+            <p>
+              <strong>Year:</strong> {artDetails?.year}
+            </p>
+            <p>
+              <strong>Location:</strong> {artDetails?.location}
+            </p>
+            <p>
+              <strong>Description:</strong> {artDetails?.description}
+            </p>
+          </div>
+        )}
       </div>
       <div className="comments-section">
         <CommentsSection artditail={artditail!} />
